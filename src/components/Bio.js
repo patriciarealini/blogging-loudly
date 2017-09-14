26ac9a3307b69a9268166012b7dc84621d7fffab
patriciarealini <hello@patriciarealini.com>
2017-09-14 18:03:18 -0400

    gatsby blog init

---
+++ src/components/Bio.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <p
+        style={{
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Patricia Realini`}
+          style={{
+            float: 'left',
+            marginRight: rhythm(1 / 4),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        internet ecologist + leisure enthusiast + emotional geographer // "extremely lonely and incredibly caffeinated" —The New Yorker{' '}
+        <a href="https://twitter.com/kylemathews">
+          You should follow him on Twitter
+        </a>
+      </p>
+    )
+  }
+}
+
+export default Bio
